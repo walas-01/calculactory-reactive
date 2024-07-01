f38f7fce1a00bf0f8be776c029c8b058be439725
walas-01 <cuenta0danna@gmail.com>
2024-07-01 12:59:12 -0300

    firebase hosting added (a lot of stuff)

--- public/main.jsx
+++ public/main.jsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client'
-import App from './src/App.jsx'
+import App from '../src/App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App/>)
---
+++ src/config/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCunw5oaOsIMgkyoWyGIy8k-mFnWRB6miQ",
+  authDomain: "calculactory-reactive.firebaseapp.com",
+  projectId: "calculactory-reactive",
+  storageBucket: "calculactory-reactive.appspot.com",
+  messagingSenderId: "918244436507",
+  appId: "1:918244436507:web:600751e8d9b69cce6966be"
+}
+
+const app = initializeApp(firebaseConfig)
