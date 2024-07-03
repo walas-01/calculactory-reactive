15f31fa03ba94949325ad9a391da2993a697ce13
walas-01 <cuenta0danna@gmail.com>
2024-07-03 15:13:10 -0300

    restored vite.config, it didn't worked

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
-  },
+  plugins: [react()]
 });
