0b914d6757fc74eb79e48fb02ae18934ced65bdc
walas-01 <cuenta0danna@gmail.com>
2024-07-03 12:36:41 -0300

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins:[
-      react([
-        'public/index.html'
-      ])
+      react({template: { transformAssetUrls: {includeAbsolute: false,},},})
     ]
 })
+
+
+
+
